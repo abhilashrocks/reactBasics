db97d337d533349966d468fda28ce4f5906f997c
AbhilashRocks <abhilash.tatikonda@focalcxm.com>
2020-05-17 11:49:48 +0530

    changes

--- reactjsProject/reactdemo/src/App.js
+++ reactjsProject/reactdemo/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
+import Search from './components/users/search';
+class App extends React.Component {
+  name = "veeresh";
+  state = {
+    users: [],
+    loading: true
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async componentDidMount() {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/users?clien_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_scrent=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data, loading: false })
+  }
+
+  searchUsers = async (text) => {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&clien_id=
+  ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_scrent=
+  ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data.items, loading: false })
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="GitHub Finder" icon="fab fa-github" />
+        <Search searchUsers={this.searchUsers} />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
